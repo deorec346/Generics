bc64c6b5b660545d3a18d898ba248e7c64562f46
Chetan Deore <deorec346@gmail.com>
2022-04-18 00:22:43 +0530

    Generics

---
+++ GenericsProb/MaxNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProb
+{
+    internal class MaxNumber
+    {
+        //Method Used To Find Integer Max Number
+        public static void FindMaxNum(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num2) >= 0)
+                Console.WriteLine("Max Value is {0}", num1);
+            else if (num2.CompareTo(num3) >= 0)
+                Console.WriteLine("Max Value is {0}", num2);
+            else
+                Console.WriteLine("Max Value is {0}", num3);
+        }
+
+        //Method Used To Find Float Max Number
+        public static void FindFloatMaxNum(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num2) >= 0)
+                Console.WriteLine("Max Value is {0}", num1);
+            else if (num2.CompareTo(num3) >= 0)
+                Console.WriteLine("Max Value is {0}", num2);
+            else
+                Console.WriteLine("Max Value is {0}", num3);
+        }
+
+    }
+}
---
+++ GenericsProb/TestCases.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProb
+{
+     internal class TestCases
+     {
+        public static readonly int[] IntCase1;
+        public static readonly int[] IntCase2;
+        public static readonly int[] IntCase3;
+        public static readonly float[] FloatCase1;
+        public static readonly float[] FloatCase2;
+        public static readonly float[] FloatCase3;
+
+
+
+        static TestCases()
+        {
+            IntCase1 = new int[] { 8, 10, 2 };
+            IntCase2 = new int[] { 2, 40, 50 };
+            IntCase3 = new int[] { 50, 30, 20 };
+            FloatCase1 = new float[] { 2.5f, 3.8f, 4.5f };
+            FloatCase2 = new float[] { 9.6f, 5.5f, 2.8f };
+            FloatCase3 = new float[] { 5.6f, 10.5f, 2.6f };
+
+        }
+
+        public static string GetString<T>(T[] testCase)
+        {
+            return $"[{testCase[0]}, {testCase[1]}, {testCase[2]}]";
+        }
+    }
+}

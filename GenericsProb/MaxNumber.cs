ad87ea719aabd45b6ac175eddfe34a4357fa4cee
Chetan Deore <deorec346@gmail.com>
2022-04-18 00:17:59 +0530

    Generics

---
+++ GenericsProb/MaxNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProb
+{
+    internal class MaxNumber
+    {
+        //Method Used To Find Integer Max Number
+        public static void FindMaxNum(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num2) >= 0)
+                Console.WriteLine("Max Value is {0}", num1);
+            else if (num2.CompareTo(num3) >= 0)
+                Console.WriteLine("Max Value is {0}", num2);
+            else
+                Console.WriteLine("Max Value is {0}", num3);
+        }
+    }
+}
---
+++ GenericsProb/TestCases.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProb
+{
+     internal class TestCases
+     {
+            public static readonly int[] IntCase1;
+            public static readonly int[] IntCase2;
+            public static readonly int[] IntCase3;
+
+
+            static TestCases()
+            {
+                IntCase1 = new int[] { 8, 10, 2 };
+                IntCase2 = new int[] { 2, 40, 50 };
+                IntCase3 = new int[] { 50, 30, 20 };
+            }
+
+            public static string GetString<T>(T[] testCase)
+            {
+                return $"[{testCase[0]}, {testCase[1]}, {testCase[2]}]";
+            }
+     }
+}
